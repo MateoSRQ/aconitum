a69a0a79364379540459c0cc14107a9cbb274834
Administrator <sanroman_me@hotmail.com>
2019-03-10 00:37:02 -0500

    Pull this

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import loglevel from "loglevel"
+import index from "./js/index"
+
+import "semantic-ui-less/semantic.less"
+import styles from './styles/index.scss'
+
+loglevel.setLevel('info')
+loglevel.info('src/index')
