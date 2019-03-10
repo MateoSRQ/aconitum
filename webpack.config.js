4e6b4a1eaa81ba08ae0387acafd6b503c3327ae8
Administrator <sanroman_me@hotmail.com>
2019-03-09 23:12:38 -0500

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer')
+const path = require('path')
+
+
+module.exports = {
+    resolve: {
+        alias: {
+            '../../theme.config$': path.join(__dirname, 'theme/theme.config')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            localIdentName: "[local]___[hash:base64:5]"
+                        }
+                    },
+                    { loader: "postcss-loader",
+                        options: {
+                            ident: 'postcss',
+                            sourceMap: true,
+                            plugins: () => [
+                                autoprefixer({
+                                    browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
+                                })
+                            ]
+                        }
+                    },
+                    { loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                use: 'file-loader?name=[name].[ext]?[hash]'
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                use: 'file-loader?name=[name].[ext]?[hash]'
+            },
+            {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.otf(\?.*)?$/,
+                use: 'file-loader?name=/fonts/[name].  [ext]&mimetype=application/font-otf'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
